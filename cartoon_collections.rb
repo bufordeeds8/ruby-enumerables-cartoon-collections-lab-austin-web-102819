5215a94b4ebdde2b1617fb2c95e4229d793c94b8
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 21:45:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(array)
   array.any? { |e| e.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.include?("cheddar", "gouda", "camembert")
+  
+  # cheese_types = ["cheddar", "gouda", "camembert"]
 end
