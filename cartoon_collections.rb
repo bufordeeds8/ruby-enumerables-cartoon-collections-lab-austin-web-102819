bcc2bcbb7579f13654001bccf29691d83325188f
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 21:51:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find(cheese_types)
+  array.find{|cheese| cheese_types.include?(cheese)}
 end
