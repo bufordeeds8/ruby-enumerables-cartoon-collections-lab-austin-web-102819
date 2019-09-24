5ccc959db6e0fe0a7f1b9a378abb018fd493a2e6
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 21:38:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ array.map { |e| p e.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
-  array.any? { |e| e > 4 }
+  array.any? { |e| e.length > 4 }
 end
 
 def find_the_cheese# code an argument here
