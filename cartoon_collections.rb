fda91b926fa6e51d816076d8b17ce6381ac84a69
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 21:26:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,6 @@ end
 # roll_call_dwarves(dwarves)
 def summon_captain_planet(array) # code an argument here
 array.map { |e| p e.capitalize + "!"}
-
 end
 
 def long_planeteer_calls# code an argument here
